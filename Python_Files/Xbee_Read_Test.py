a632f64f321054a0d0148956126b53d975c6981a
Timothy Anglea <mathman93@gmail.com>
2021-06-05 23:18:44 -0400

    Initial commit - 6/5
    Initial Data - will remove some stuff.

---
+++ Python_Files/Xbee_Read_Test.py
@@ -0,0 +1,73 @@
+''' Xbee_Read_Test.py
+Purpose: Testing communication between Xbee modules on separate RPi
+IMPORTANT: Must be run using Python 3 (python3)
+Last Modified: 2/11/2020
+'''
+## Import libraries ##
+import serial
+import time
+import RPi.GPIO as GPIO
+
+## Variables and Constants ##
+global Xbee # Specifies connection to Xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200) # Baud rate should be 115200
+# LED pin numbers
+yled = 5
+rled = 6
+gled = 13
+
+## Functions and Definitions ##
+''' Displays current date and time to the screen
+	'''
+def DisplayDateTime():
+	# Month day, Year, Hour:Minute:Seconds
+	date_time = time.strftime("%B %d, %Y, %H:%M:%S", time.gmtime())
+	print("Program run: ", date_time)
+
+## -- Code Starts Here -- ##
+# Setup Code #
+GPIO.setmode(GPIO.BCM) # Use BCM pin numbering for GPIO
+DisplayDateTime()
+
+# LED Pin setup
+GPIO.setup(yled, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(rled, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(gled, GPIO.OUT, initial=GPIO.LOW)
+
+# Main Code #
+sendtime = time.time()
+sendtime_offset = 1.0 # Time between sending messages
+basetime = time.time()
+basetime_offset = 0.5 # Time between LED blinks
+
+
+while True:
+	try:
+		if (time.time() - sendtime) > sendtime_offset:
+			message = '1' # Change this to any character string you want
+			Xbee.write(message.encode()) # Send the number over the Xbee
+			print("you sent stuff")
+			sendtime += sendtime_offset # Increase offset for next time to send message
+		
+		if Xbee.inWaiting() > 0: # If there is something in the receive buffer
+			message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
+			print(message) # To see what the message is
+		
+		# LED blink conditional, to make sure code is running
+		if (time.time() - basetime) > basetime_offset: # If enough time has passed.
+			if GPIO.input(gled) == True:  # If the LED is on...
+				GPIO.output(gled, GPIO.LOW)  # turn it off
+			else:
+				GPIO.output(gled, GPIO.HIGH) # otherwise, turn it on.
+			basetime += basetime_offset  # set the next base time
+		
+	except KeyboardInterrupt:
+		print('')
+		break
+
+## -- Ending Code Starts Here -- ##
+# Make sure this code runs to end the program cleanly
+GPIO.output(gled, GPIO.LOW) # Turn off green LED
+
+Xbee.close()
+GPIO.cleanup() # Reset GPIO pins for next program
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# SPRI2021_Roomba
+# SPRI2020_Roomba
+Repository for Python code used on Roombas for SPRI - Summer 2020.
+
+Welcome to the Summer Program for Research Interns (SPRI) 2019 at Clemson University. We will be using GitHub to organize and manage our Python code and other files for the project. GitHub allows us to incorporate necessary changes and updates from multiple users at the same time. Please become familiar with the layout and workflow of GitHub. Let me (mathman93) know if you have any questions.
