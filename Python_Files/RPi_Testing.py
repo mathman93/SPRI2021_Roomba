5482793ae7060bdd34d597c7d803531cc653f9e1
Timothy Anglea <mathman93@gmail.com>
2021-06-11 09:40:59 -0400

    some testing

---
+++ Python_Files/RPi_Testing.py
@@ -0,0 +1,44 @@
+''' RPi_Testing.py
+Purpose: Basic code for running Xbee, and illustrating code behavior.
+	Sets up Xbee;
+IMPORTANT: Must be run using Python 3 (python3)
+Last Modified: 6/11/2021
+'''
+## Import libraries ##
+import serial
+import time
+import RPi.GPIO as GPIO
+
+## Variables and Constants ##
+global Xbee # Specifies connection to Xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200) # Baud rate should be 115200
+
+## Functions and Definitions ##
+''' Displays current date and time to the screen
+	'''
+def DisplayDateTime():
+	# Month day, Year, Hour:Minute:Seconds
+	date_time = time.strftime("%B %d, %Y, %H:%M:%S", time.gmtime())
+	print("Program run: ", date_time)
+
+## -- Code Starts Here -- ##
+# Setup Code #
+GPIO.setmode(GPIO.BCM) # Use BCM pin numbering for GPIO
+DisplayDateTime() # Display current date and time
+
+# Clear out Xbee message buffer.
+if Xbee.inWaiting() > 0: # If anything is in the Xbee receive buffer
+	x = Xbee.read(Xbee.inWaiting()).decode() # Clear out Xbee input buffer
+	#print(x) # Include for debugging
+
+phase = float(input("Initial oscillator phase? ")) # What are the units of 'phase'?
+print("Initial phase value: {0}".format(phase))
+
+# Main Code #
+
+
+## -- Ending Code Starts Here -- ##
+# Make sure this code runs to end the program cleanly
+
+Xbee.close()
+GPIO.cleanup() # Reset GPIO pins for next program
