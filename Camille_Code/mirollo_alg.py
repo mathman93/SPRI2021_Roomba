18c96c2cc72c0ea159fe8fb08e5ded29415de4b3
Katie Day <71191376+katcday@users.noreply.github.com>
2021-06-25 10:55:16 -0400

    Update mirollo_alg.py

--- Camille_Code/mirollo_alg.py
+++ Camille_Code/mirollo_alg.py
@@ -5,6 +5,52 @@
 
 import math
 
+''' RPi_Testing.py
+Purpose: Basic code for running Xbee, and illustrating code behavior.
+	Sets up Xbee;
+IMPORTANT: Must be run using Python 3 (python3)
+Last Modified: 6/21/2021
+By: Timothy Anglea
+'''
+## Import libraries ##
+import serial # For serial port functions (e.g., USB)
+import time # For accessing system time
+import RPi.GPIO as GPIO # For IO pin access on Raspberry Pi
+import math
+
+## Variables and Constants ##
+global Xbee # Specifies connection to Xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200) # Baud rate should be 115200
+
+## Functions and Definitions ##
+''' Displays current date and time to the screen
+	'''
+def DisplayDateTime():
+	# Month day, Year, Hour:Minute:Seconds
+	date_time = time.strftime("%B %d, %Y, %H:%M:%S", time.gmtime())
+	print("Program run: ", date_time)
+
+## -- Code Starts Here -- ##
+# Setup Code #
+GPIO.setmode(GPIO.BCM) # Use BCM pin numbering for GPIO
+DisplayDateTime() # Display current date and time
+
+while True:
+	try:
+		initial_phase = float(input("Initial oscillator phase? ")) # What are the units of 'phase'?
+		print("Initial phase value: {0} degrees".format(initial_phase))
+		break
+	except ValueError:
+		print("Not a number. Try again.")
+		continue
+# End while
+
+# Clear out Xbee message buffer.
+if Xbee.inWaiting() > 0: # If anything is in the Xbee receive buffer
+	x = Xbee.read(Xbee.inWaiting()).decode() # Clear out Xbee input buffer
+	#print(x) # Include for debugging
+# End if
+
 time_to_take = 2.0 # Length of cycle in seconds
 # frequency = 180 # degrees per second
 threshold = 360 # "degrees" (could be radians)
